e4e640bfa71ae140096c525613fba1f979054cb5
George Baskerville <GBask3r@proton.me>
2026-09-13 09:58:41 +0100

    Update pdf-loading.jsx

--- app/_components/ui/flipbook-viewer/pad-loading/pdf-loading.jsx
+++ app/_components/ui/flipbook-viewer/pad-loading/pdf-loading.jsx
@@ -80,7 +80,7 @@ const PdfLoading = () => {
                     </svg>
                 </div>
                 <div className="text-base font-semibold">
-                    Loading PDF...
+                    Loading cookbook...
                 </div>
             </div>
         </div>
